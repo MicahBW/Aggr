d6ad7efaaee432656e121df07e6bc0b45763c836
MicahBW <m.b.wolfsohn@gmail.com>
2018-09-11 19:25:03 -0500

    Initial Commit

---
+++ Aggr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aggr
+//
+//  Created by Micah Byrne Wolfsohn on 9/11/18.
+//  Copyright © 2018 AggrCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
