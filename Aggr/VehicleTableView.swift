b202046deb87e10f0bbf91048144f824dae7c12d
MicahBW <m.b.wolfsohn@gmail.com>
2018-09-24 18:41:47 -0500

    Prior to messing with tableviewcell

---
+++ Aggr/VehicleTableView.swift
@@ -0,0 +1,23 @@
+//
+//  VehicleTableView.swift
+//  Aggr
+//
+//  Created by Micah Byrne Wolfsohn on 9/22/18.
+//  Copyright © 2018 AggrCo. All rights reserved.
+//
+
+import UIKit
+
+class VehicleTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    
+
+}
---
+++ Aggr/VehicleTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  VehicleTableViewCell.swift
+//  Aggr
+//
+//  Created by Micah Byrne Wolfsohn on 9/22/18.
+//  Copyright © 2018 AggrCo. All rights reserved.
+//
+
+import UIKit
+
+class VehicleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imgView: UIImageView!
+    // TODO:
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
